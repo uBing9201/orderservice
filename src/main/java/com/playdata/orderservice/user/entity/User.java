fb0222c928a94781dd5473fe80e6bdfa42ad411d
ubing <ubing0101@kakao.com>
2025-04-22 11:01:29 +0900

    User Entity 생성

---
+++ src/main/java/com/playdata/orderservice/user/entity/User.java
@@ -0,0 +1,42 @@
+package com.playdata.orderservice.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_user")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default // builder 패턴 사용해서 객체 초기화 시 전달한 값으로 세팅해 주라.
+    private Role role = Role.USER;
+}
