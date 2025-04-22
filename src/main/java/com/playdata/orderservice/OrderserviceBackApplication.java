b82e52ee24e099cb956baa483af6b5ca4c75f6d4
ubing <ubing0101@kakao.com>
2025-04-22 10:52:52 +0900

    initial commit

---
+++ src/main/java/com/playdata/orderservice/OrderserviceBackApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserviceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderserviceBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playdata/orderservice/OrderserviceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
