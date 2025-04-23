20c911d11f52a98806144a4d88780941e9821c10
ubing <ubing0101@kakao.com>
2025-04-23 10:56:23 +0900

    JwtAuthFilter 작성 및 Security 설정완료. 요청 검증 로직 동작 확인 중 (myInfo)

--- src/main/java/com/playdata/orderservice/common/configs/SecurityConfig.java
+++ src/main/java/com/playdata/orderservice/common/configs/SecurityConfig.java
@@ -1,46 +1,53 @@
 package com.playdata.orderservice.common.configs;
 
+import com.playdata.orderservice.common.auth.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity // 권한 검사를 컨트롤러의 메서드에서 전역적으로 수행하기 위한 설정.
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    // 필터 등록을 위해서 객체가 필요 -> 빈 등록된 객체를 자동 주입.
+    private final JwtAuthFilter jwtAuthFilter;
 
     // 시큐리티 기본 설정 (권한 처리, 초기 로그인 화면 없애기 등등...)
     @Bean // 이 메서드가 리턴하는 시큐리티 설정을 빈으로 등록하겠다.
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         // 스프링 시큐리티에서 기본으로 제공하는 CSRF 토큰 공격을 방지하기 위한 장치 해제.
         // CSRF(Cross Site Request Forgery) 사이트 간 요청 위조
-        // http.csrf(csrf->csrf.disable());
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(csrf -> csrf.disable());
 
         // 세션 관리 상태를 사용하지 않고
-        // STATELESS 한 토큰을 사용하겠다.
-        // 해당 세션내용을 끄지 않는다면 html 페이지를 만들어 로그인을 하도록 페이지를만들어준다
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        // STATELESS한 토큰을 사용하겠다.
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        // 요청 권한 설정 (어떤 url이냐에 따라 검사를 할지 말지 결정)
+        // 요청 권한 설정 (어떤 url이냐에 따라 검사를 할 지 말지를 결정)
         http.authorizeHttpRequests(auth -> {
             auth
                     .requestMatchers("/user/create", "/user/doLogin").permitAll()
                     .anyRequest().authenticated();
         });
         // "/user/create", "/user/doLogin"은 인증 검사가 필요 없다고 설정했고,
-        // 나머지 요청들은 권한 검사가 필요하다고 세팅
+        // 나머지 요청들은 권한 검사가 필요하다고 세팅 했습니다.
         // 권한 검사가 필요한 요청들을 어떤 필터로 검사할지를 추가해 주면 됩니다.
 
+        // 스프링 시큐리티가 기본으로 세팅하는 여러가지 필터가 있는데요,
+        // 내가 직접 만든 커스텀 필터가 해당 필터를 대체할 것이기 때문에, 그 필터 앞에 세워놓는 겁니다.
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-
-        return null;
+        // 설정한 HttpSecurity 객체를 기반으로 시큐리티 설정 구축 및 반환.
+        return http.build();
     }
 
 }
--- src/main/java/com/playdata/orderservice/user/controller/UserController.java
+++ src/main/java/com/playdata/orderservice/user/controller/UserController.java
@@ -10,11 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user") // user 관련 요청은 /user로 시작한다고 가정.
@@ -22,8 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     // 컨트롤러는 서비스에 의존하고 있다. (요청과 함께 전달받은 데이터를 서비스에게 넘겨야 함!)
+    // 빈 등록된 서비스 객체를 자동으로 주입 받자!
     private final UserService userService;
-
     private final JwtTokenProvider jwtTokenProvider;
 
     /*
@@ -44,33 +40,39 @@ public class UserController {
         // 화면단에서 전달된 데이터를 DB에 넣자.
         // 혹시 이메일이 중복되었는가? -> 이미 이전에 회원가입을 한 회원이라면 거절.
         // dto를 DB에 바로 때려? -> dto를 entity로 바꾸는 로직 추가.
-        User saved = userService.userCreate(dto);
 
-        // ResponseEntity는 응답을 줄 때 다양한 정보를 한번에 포장해서 넘길 수 있다.
-        // 요청에 따른 응답 상태 코드, 응답 헤더에 정보를 추가, 일관된 응답처리를 제공
-        CommonResDto resDto = new CommonResDto(HttpStatus.CREATED, "User Created", saved);
+
+        User saved = userService.userCreate(dto);
+        // ResponseEntity는 응답을 줄 때 다양한 정보를 한번에 포장해서 넘길 수 있습니다.
+        // 요청에 따른 응답 상태 코드, 응답 헤더에 정보를 추가, 일관된 응답 처리를 제공합니다.
+
+        CommonResDto resDto
+                = new CommonResDto(HttpStatus.CREATED,
+                "User Created", saved.getName());
+
         return new ResponseEntity<>(resDto, HttpStatus.CREATED);
     }
 
     @PostMapping("/doLogin")
     public ResponseEntity<?> doLogin(@RequestBody UserLoginReqDto dto) {
-
         User user = userService.login(dto);
 
         // 회원 정보가 일치한다면 -> 로그인 성공.
         // 로그인 유지를 해 주고 싶다.
-        // 백엔드는 요청이 들어왔을 때 이 사람이 이전에 로그인 한 사람인지 알 수 없다.
-        // 징표를 하나 만들어 넘겨준다 -> JWT를 발급해서 클라이언트에게 전달해주겠다
-
-        String token = jwtTokenProvider.createToken(user.getEmail(), user.getRole());
-        CommonResDto resDto = new CommonResDto(HttpStatus.OK, "로그인 성공", token);
-
+        // 백엔드는 요청이 들어왔을 때 이 사람이 이전에 로그인 성공 한 사람인지 알 수가 없다.
+        // 징표를 하나 만들어 주겠다. -> JWT를 발급해서 클라이언트에게 전달해 주겠다!
+        String token
+                = jwtTokenProvider.createToken(user.getEmail(), user.getRole().toString());
+        CommonResDto resDto
+                = new CommonResDto(HttpStatus.OK,
+                "Login Success", token);
         return new ResponseEntity<>(resDto, HttpStatus.OK);
     }
 
-    // 회원 정보 조회 (마이페이지) -> 로그인 한 회원만이 요청할 수 있다.
+    // 회원 정보 조회 (마이페이지) -> 로그인 한 회원만이 요청할 수 있습니다.
     @GetMapping("/myInfo")
     public ResponseEntity<?> getMyInfo() {
+        System.out.println("/user/myInfo: GET!");
         return null;
     }
 
