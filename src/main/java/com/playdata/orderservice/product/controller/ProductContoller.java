94d55afad5d27faef96022a381980b60f48b54c5
ubing <ubing0101@kakao.com>
2025-04-28 11:56:08 +0900

    검색 조건과 입력값에 맞는 분기별 페이징 로직 작성

--- src/main/java/com/playdata/orderservice/product/controller/ProductContoller.java
+++ src/main/java/com/playdata/orderservice/product/controller/ProductContoller.java
@@ -3,9 +3,9 @@ package com.playdata.orderservice.product.controller;
 import com.playdata.orderservice.common.dto.CommonResDto;
 import com.playdata.orderservice.product.dto.ProductResDto;
 import com.playdata.orderservice.product.dto.ProductSaveReqDto;
+import com.playdata.orderservice.product.dto.ProductSearchDto;
 import com.playdata.orderservice.product.entity.Product;
 import com.playdata.orderservice.product.service.ProductService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -17,19 +17,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/product")
 @RequiredArgsConstructor
 @Slf4j
-public class ProductController {
+public class ProductContoller {
 
     private final ProductService productService;
 
     // 상품 등록 요청
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
-    public ResponseEntity<?> productCreate(ProductSaveReqDto dto) {
-
+    public ResponseEntity<?> createProduct(ProductSaveReqDto dto) {
         /*
         상품 등록 요청은 여러 데이터와 함께 이미지가 전달될 것입니다.
         1. JS의 formData 객체를 통해 모든 데이터를 전달 (JSON 형태가 아니라, multipart/form-data 형식)
@@ -41,22 +42,27 @@ public class ProductController {
         log.info("dto: {}", dto);
         Product product = productService.productCreate(dto);
 
-        CommonResDto resDto =  new CommonResDto(HttpStatus.CREATED, "상품 등록 성공", product);
+        CommonResDto resDto
+                = new CommonResDto(HttpStatus.CREATED, "상품 등록 성공", product.getId());
 
         return new ResponseEntity<>(resDto, HttpStatus.CREATED);
     }
 
     // 요청방식: GET, 요청 URL: /product/list
-    // 페이징이 필요합니다. 리턴은 ProductResDto 형태로 리턴됩니다.
+    // 따로 권한은 필요 없습니다. (누구나 요청이 가능합니다. 로그인 안해도 됩니다.)
+    // 페이징이 필요합니다. -> 클라이언트 쪽에서 페이지 번호와 한 화면에 보여질 상품 개수, 정렬 방식이 넘어와요.
+    // 리턴은 ProductResDto 형태로 리턴됩니다.
     // ProductResDto(id, name, category, price, stockQuantity, imagePath)
     @GetMapping("/list")
-    public ResponseEntity<?> getAll(Pageable pageable){
-        // 페이지 번호를 number로 주시면 안됨! page 로 전달해 주어야 한다.
-        // 사용자가 선택한 페이지 번호 -1을 클라이언트 단에서 해서 전달해 주어야 한다
-        List<ProductResDto> dtoList = productService.productList(pageable);
-        log.info("product: {}", dtoList);
+    public ResponseEntity<?> listProduct(ProductSearchDto dto, Pageable pageable) {
+        // 페이지 번호를 number로 주시면 안됨! page로 전달해 주셔야 합니다!
+        // 사용자가 선택한 페이지 번호 -1을 클라이언트 단에서 해서 전달해 주셔야 합니다.
+        log.info("/product/list: GET, pageable: {}", pageable);
+        log.info("dto: {}", dto);
+        List<ProductResDto> dtoList = productService.productList(dto, pageable);
 
-        CommonResDto resDto = new CommonResDto(HttpStatus.OK, "상품 리스트 조회 성공", dtoList);
+        CommonResDto resDto
+                = new CommonResDto(HttpStatus.OK, "상품 리스트 정상 조회", dtoList);
 
         return ResponseEntity.ok().body(resDto);
     }
--- src/main/java/com/playdata/orderservice/product/service/ProductService.java
+++ src/main/java/com/playdata/orderservice/product/service/ProductService.java
@@ -2,13 +2,9 @@ package com.playdata.orderservice.product.service;
 
 import com.playdata.orderservice.product.dto.ProductResDto;
 import com.playdata.orderservice.product.dto.ProductSaveReqDto;
+import com.playdata.orderservice.product.dto.ProductSearchDto;
 import com.playdata.orderservice.product.entity.Product;
 import com.playdata.orderservice.product.repository.ProductRepository;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -16,6 +12,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,13 +30,20 @@ public class ProductService {
     public Product productCreate(ProductSaveReqDto dto) {
         // 원본 이미지를 어딘가에 저장하고, 그 저장된 위치를 Entity에 세팅하자.
         MultipartFile productImage = dto.getProductImage();
-        UUID uuid = UUID.randomUUID();
-        String uniqueFileName = UUID.randomUUID() + "_" + productImage.getOriginalFilename();
-        File file = new File("/Users/ubing/Desktop/playdata/update/" + uniqueFileName);
-        try{
+
+        // 상품을 등록하는 과정에서, 이미지 이름의 충돌이 발생할 수 있기 때문에
+        // 랜덤한 문자열을 섞어서 파일 중복을 막아주자.
+        String uniqueFileName
+                = UUID.randomUUID() + "_" + productImage.getOriginalFilename();
+
+        // 특정 로컬 경로에 이미지를 전송하고, 그 경로를 Entity에 세팅하자.
+        File file
+                = new File("/Users/stephen/Desktop/playdata_8th_develop/upload/" + uniqueFileName);
+
+        try {
             productImage.transferTo(file);
         } catch (IOException e) {
-            throw new RuntimeException("파일 저장 에러");
+            throw new RuntimeException("이미지 저장 실패!");
         }
 
         Product product = dto.toEntity();
@@ -44,14 +53,20 @@ public class ProductService {
 
     }
 
-    public List<ProductResDto> productList(Pageable pageable) {
-        Page<Product> products = productRepository.findAll(pageable);
+    public List<ProductResDto> productList(ProductSearchDto dto, Pageable pageable) {
+        Page<Product> products;
+        if (dto.getCategory() == null) {
+            products = productRepository.findAll(pageable);
+        } else if (dto.getCategory().equals("name")) {
+            products = productRepository.findByNameValue(dto.getSearchName(), pageable);
+        } else {
+            products = productRepository.findByCategoryValue(dto.getSearchName(), pageable);
+        }
 
-        List<Product> content = products.getContent();
-        List<ProductResDto> dtoList = content.stream()
+        List<Product> productList = products.getContent();
+
+        return productList.stream()
                 .map(Product::fromEntity)
                 .collect(Collectors.toList());
-
-        return dtoList;
     }
 }
